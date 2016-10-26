8022112c892ee950a2abcd2f8f2cee56df8d69b7
William <13609057966@163.com>
2016-10-26 02:44:44 -0400

    Add function Display answer and other small changes

--- Random_Quiz.py
+++ Random_Quiz.py
@@ -1,52 +1,61 @@
-Easy_Quizzes = ["Gala, Jonagold and Pink Lady are varieties of __________.",
-				"__________ was named after the Italian city of Venice ",
-				"If you were puting numbers on new changing room lockers to be numbered from 1 to 100, you would use the number 9, __________ times.",
-				"In the Star Wars films, __________ and __________ played Obi Wan Kenobi"]
-Blank = "__________"
+Blank = "_________________"
+
+Easy_Quizzes = ["Gala, Jonagold and Pink Lady are varieties of " + Blank + " .",
+				Blank + " was named after the Italian city of Venice ",
+				"If you were puting numbers on new changing room lockers to be numbered from 1 to 100 , you would use the number 9 , " + Blank + " times .",
+				"In the Star Wars films , " + Blank + " and " + Blank + "played Obi Wan Kenobi ."]
 
 
 Easy_Answers =[["APPLE"], ["VENEZUELA"], ["20"], ["ALEC GUINESS", "EWAN MCGREGOR"]]
 
-def Answer_Match(mode, question_count, blank):
+def Answer_Match(mode, answer, answer_index):
 	if mode == "E":
-		return Easy_Answers[question_count][blank]
+		return Easy_Answers[answer][answer_index]
+
 	
-def Number_Of_Blanks(splitted_string):
-	count = 0
-	for pos in splitted_string:
-		if pos == Blank:
-			count += 1
-			return count
+#def Number_Of_Blanks(splitted_string):
+	#count = 0
+	#for pos in splitted_string:
+		#if pos == Blank:
+			#count += 1
+			#return count
 
-def Prompt_User(blank, answer):
-	user_input = raw_input("Please fill in blank " + (str)(blank) + ": ")
+def Prompt_User(blank_index, answer):
+	user_input = raw_input("Please fill in blank " + (str)(blank_index) + ": ")
 	while user_input != answer:
-		user_input = raw_input("Please try again. Fill in blank " + (str)(blank) + ": ")
+		user_input = raw_input("Please try again. Fill in blank " + (str)(blank_index) + ": ")
+	print "Correct!"
 
 
-def Select_Quiz(mode,Quiz):
-	question_count = 0
+def Display_Answers(new_string, question, answer):
+	join_string = " ".join(new_string)
+	string_index = join_string.find(answer)
+	space = len(Blank)
+	string_with_answer = J= join_string  + question[string_index + space:]
+	print string_with_answer
+
+
+
+def Select_Quiz(mode,quiz):
+	question_index = 0
 		
-	for question in Quiz: 
-		print "QUESTION " + (str)(question_count) + " : "
+	for question in quiz: 
+		print "QUESTION " + (str)(question_index) + " : "
 		print question
 
 		splitted_string = question.split()	
 		new_string = []
-		blank = 0
+		blank_index = 0	
 		for word in splitted_string:
 			if Blank not in word:
 				new_string.append(word)
 			else: 
-				answer = Answer_Match(mode, question_count, blank)
-				Prompt_User(blank, answer)
-				word = word.replace(Blank, answer)
-				new_string.append(word)
-				blank += 1
-		new_string = " ".join(new_string)
-		print new_string + "\n"
-			
-		question_count += 1
+				answer = Answer_Match(mode, question_index, blank_index)
+				Prompt_User(blank_index, answer)
+				new_string.append(answer)
+				Display_Answers(new_string, question, answer)
+				blank_index += 1
+		question_index += 1
 
 def Random_Quiz():
 	print "Welcome to random quiz! Please select a difficulty"
